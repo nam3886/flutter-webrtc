69d0d65cdb82192c2b80be04c96dbb7d688ea8b4
nam3886 <43193144+nam3886@users.noreply.github.com>
2025-12-06 14:55:53 +0700

    Add null check for FlutterEventSink in postEvent

--- common/darwin/Classes/FlutterWebRTCPlugin.m
+++ common/darwin/Classes/FlutterWebRTCPlugin.m
@@ -93,7 +93,9 @@ NSArray<RTC_OBJC_TYPE(RTCVideoCodecInfo) *>* motifyH264ProfileLevelId(
 
 void postEvent(FlutterEventSink _Nonnull sink, id _Nullable event) {
     dispatch_async(dispatch_get_main_queue(), ^{
-      sink(event);
+      if (sink) {
+        sink(event);
+    }
     });
 }
 
